2793e6a1918b4f0e82a52696a53cf3906fcd72c9
lSara-MM <sqmartinezm@gmail.com>
2024-10-03 14:14:35 +0200

    pruebas (no se si da errores criticos)

---
+++ TV3-Premis-Ludi-Teatre/Assets/Scripts/Prop.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+//https://gamedevbeginner.com/how-to-change-a-sprite-from-a-script-in-unity-with-examples/
+//https://gamedevbeginner.com/addressable-assets-in-unity/
+
+public class PropData
+{
+    public string name { get; set; }
+    public string internalTag { get; set; }
+    public string imagePath { get; set; }
+
+    public Prop CreateProp(string name, string internalTag, string imgPath)
+    {
+        if (true)   // validation?
+        {
+            Prop prop = new Prop(this.name, this.internalTag, this.imagePath);
+            return prop;
+        }
+
+        return null;
+    }
+}
+
+public class Prop
+{
+    public string name;
+    public string internalTag;
+    public string imagePath;
+
+    public Prop()
+    {
+
+    }
+    public Prop(string name, string internalTag, string imgPath)
+    {
+
+    }
+}
